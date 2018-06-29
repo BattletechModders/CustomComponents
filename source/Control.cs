e91f758a5591a3a305bb703eae009bd728caa326
Denadan <Denadan.sa@gmail.com>
2018-06-29 09:02:21 +0300

    Initial

---
+++ source/Control.cs
@@ -0,0 +1,32 @@
+﻿using DynModLib;
+using Harmony;
+using System;
+using System.Reflection;
+
+namespace CustomComponents
+{
+    public static class Control
+    {
+        internal static Mod mod;
+
+        public static void Init(string directory, string settingsJSON)
+        {
+            mod = new Mod(directory);
+            try
+            {
+                //   mod.LoadSettings(settings);
+
+                var harmony = HarmonyInstance.Create("io.github.denadan.CustomComponents");
+                harmony.PatchAll(Assembly.GetExecutingAssembly());
+
+                // logging output can be found under BATTLETECH\BattleTech_Data\output_log.txt
+                // or also under yourmod/log.txt
+                mod.Logger.Log("Loaded " + mod.Name);
+            }
+            catch (Exception e)
+            {
+                mod.Logger.LogError(e);
+            }
+        }
+    }
+}
