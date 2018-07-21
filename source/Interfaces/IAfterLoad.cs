ffbc57e0bee88b78e21e24fdcb68bead9651b2ec
CptMoore <39010654+CptMoore@users.noreply.github.com>
2018-07-21 16:26:10 +0200

    made IAfterLoad public

--- source/Interfaces/IAfterLoad.cs
+++ source/Interfaces/IAfterLoad.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace CustomComponents
 {
-    interface IAfterLoad
+    public interface IAfterLoad
     {
         void OnLoaded(Dictionary<string, object> values);
     }
