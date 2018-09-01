9c0750571f460634c6e2c54e2f7665817021b40b
denadan <denadan.sa@gmail.com>
2018-09-01 12:59:30 +0300

    RequieredID to array

--- source/Components/RequieredID.cs
+++ source/Components/RequieredID.cs
@@ -7,17 +7,17 @@ namespace CustomComponents
     [CustomComponent("RequiredID")]
     public class RequiredID : SimpleCustomComponent, IMechValidate
     {
-        public string ComponentDefId { get; set; }
+        public string[] ComponentDefId { get; set; }
         public string ErrorMessage { get; set; } 
 
         public void ValidateMech(Dictionary<MechValidationType, List<Localize.Text>> errors, MechValidationLevel validationLevel, MechDef mechDef,
             MechComponentRef componentRef)
         {
-            if (string.IsNullOrEmpty(ComponentDefId))
+            if (ComponentDefId == null || ComponentDefId.Length == 0)
                 return;
             foreach (var cref in mechDef.Inventory)
             {
-                if (cref.ComponentDefID == ComponentDefId)
+                if (ComponentDefId.Contains(cref.ComponentDefID))
                     return;
             }
             errors[MechValidationType.InvalidInventorySlots].Add(new Localize.Text(string.IsNullOrEmpty(ErrorMessage) ? $"{Def.Description.Name} missed required components" : ErrorMessage));
@@ -25,11 +25,11 @@ namespace CustomComponents
 
         public bool ValidateMechCanBeFielded(MechDef mechDef, MechComponentRef componentRef)
         {
-            if (string.IsNullOrEmpty(ComponentDefId))
+            if (ComponentDefId == null || ComponentDefId.Length == 0)
                 return true;
             foreach (var cref in mechDef.Inventory)
             {
-                if (cref.ComponentDefID == ComponentDefId)
+                if (ComponentDefId.Contains(cref.ComponentDefID))
                     return true;
             }
             return false;
