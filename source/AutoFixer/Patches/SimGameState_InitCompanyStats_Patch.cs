68662446fd0019e4f24044bbb5b6044b69523e07
CptMoore <39010654+CptMoore@users.noreply.github.com>
2019-02-09 13:38:51 +0100

    Allow AutoFixers to initialize autofix variables on new campaign.

---
+++ source/AutoFixer/Patches/SimGameState_InitCompanyStats_Patch.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BattleTech;
+using Harmony;
+
+namespace CustomComponents
+{
+    [HarmonyPatch(typeof(SimGameState), "InitCompanyStats")]
+    public static class SimGameState_InitCompanyStats_Patch
+    {
+        public static void Postfix(SimGameState __instance)
+        {
+            try
+            {
+                AutoFixer.Shared.FixSavedMech(new List<MechDef>(), __instance);
+            }
+            catch (Exception e)
+            {
+                Control.Logger.LogError(e);
+            }
+        }
+    }
+}
