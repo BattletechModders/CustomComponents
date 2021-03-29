b274f9448603dd7eb4416d1f975e99330dfaaa24
CptMoore <39010654+CptMoore@users.noreply.github.com>
2021-03-29 22:33:40 +0200

    Added git workflow build. Updated GitVersionTask. Added script to build release zip. Added user file template.

---
+++ release.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+export PATH="/c/Program Files/7-Zip/:$PATH"
+
+SEVENZIP="7z"
+
+set -ex
+
+CCZIP="CustomComponents/dist/CustomComponents.zip"
+rm -f "$CCZIP"
+
+cd ..
+
+(
+cd CustomComponents/source
+#git describe --exact-match
+dotnet build --configuration Release --no-incremental -p:OutputPath=../ "$@"
+)
+
+(
+INCLUDES="-i!CustomComponents/CustomComponents.dll -i!CustomComponents/LICENSE -i!CustomComponents/mod.json -i!CustomComponents/mod.minimal.json -i!CustomComponents/README.md"
+
+"$SEVENZIP" a -tzip -mx9 "$CCZIP" $INCLUDES
+)
