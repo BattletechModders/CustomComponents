046ad97270f6ffca1c67b897c2354829ebb6cf4e
CptMoore <39010654+CptMoore@users.noreply.github.com>
2021-07-22 19:59:24 +0200

    QuickFix for ammo not selectable.

---
+++ source/Patches/MechLabPanel_MechCanUseAmmo_Patch.cs
@@ -0,0 +1,18 @@
+﻿using BattleTech;
+using BattleTech.UI;
+using Harmony;
+
+namespace CustomComponents.Patches
+{
+    [HarmonyPatch(typeof(MechLabPanel), "MechCanUseAmmo")]
+    internal class MechLabPanel_MechCanUseAmmo_Patch
+    {
+        private static SimGameState state;
+
+        public static bool Prefix(ref bool __result)
+        {
+            __result = true;
+            return false;
+        }
+    }
+}
