70f7e230e4d571675770270075b196ae0b448f7b
CptMoore <39010654+CptMoore@users.noreply.github.com>
2022-04-23 13:03:30 +0200

    Simplified workflow. Upgraded GitVersion.

--- release.sh
+++ release.sh
@@ -6,19 +6,17 @@ SEVENZIP="7z"
 
 set -ex
 
-CCZIP="CustomComponents/dist/CustomComponents.zip"
-rm -f "$CCZIP"
-
-cd ..
-
 (
-cd CustomComponents/source
-#git describe --exact-match
+cd source
 dotnet build --configuration Release --no-incremental -p:OutputPath=../ "$@"
 )
 
 (
+cd ..
+
+CCZIP="CustomComponents/dist/CustomComponents.zip"
 INCLUDES="-i!CustomComponents/CustomComponents.dll -i!CustomComponents/LICENSE -i!CustomComponents/mod.json -i!CustomComponents/mod.minimal.json -i!CustomComponents/README.md -ir!CustomComponents/data"
 
+rm -f "$CCZIP"
 "$SEVENZIP" a -tzip -mx9 "$CCZIP" $INCLUDES
 )
