6e12f49907ef094ac618becdb4f6892d3d1b5981
CptMoore <39010654+CptMoore@users.noreply.github.com>
2023-01-11 03:40:23 +0100

    Added support for weapons-only stripping.

---
+++ source/Helpers/InputHelper.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+namespace CustomComponents;
+
+internal static class InputHelper
+{
+    internal static bool IsControlPressed => Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl);
+}
--- source/InventoryOperations/Patches/MechLabLocationWidget_StripEquipment_Patch.cs
+++ source/InventoryOperations/Patches/MechLabLocationWidget_StripEquipment_Patch.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using BattleTech;
 using BattleTech.UI;
 using CustomComponents.Changes;
 using Harmony;
@@ -14,9 +15,17 @@ internal static class MechLabLocationWidget_StripEquipment_Patch
         Log.InventoryOperations.Trace?.Log($"StripEquipment in {__instance.loadout.Location}");
         var lhelper = MechLabHelper.CurrentMechLab.GetLocationHelper(__instance.loadout.Location);
 
+        var controlKeyPressed = InputHelper.IsControlPressed;
+
         var changes = new Queue<IChange>();
 
         foreach (var item in lhelper.LocalInventory)
+        {
+            if (controlKeyPressed && item.ComponentRef.ComponentDefType != ComponentType.Weapon)
+            {
+                continue;
+            }
+
             if (!item.ComponentRef.IsFixed)
             {
                 changes.Enqueue(new Change_Remove(item.ComponentRef.ComponentDefID, __instance.loadout.Location));
@@ -26,6 +35,7 @@ internal static class MechLabLocationWidget_StripEquipment_Patch
             {
                 Log.InventoryOperations.Trace?.Log($"- fixed {item.ComponentRef.ComponentDefID}");
             }
+        }
 
         var state = new InventoryOperationState(changes, MechLabHelper.CurrentMechLab.ActiveMech);
         state.DoChanges();
