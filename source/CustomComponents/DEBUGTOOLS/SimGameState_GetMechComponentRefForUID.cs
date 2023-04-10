0bec0cbc033e163ac81e7f4ed78ea83b2d0ff6d1
CptMoore <39010654+CptMoore@users.noreply.github.com>
2023-04-10 11:07:22 +0200

    Fix for mech argument being null.

--- source/CustomComponents/DEBUGTOOLS/SimGameState_GetMechComponentRefForUID.cs
+++ source/CustomComponents/DEBUGTOOLS/SimGameState_GetMechComponentRefForUID.cs
@@ -3,7 +3,7 @@
 namespace CustomComponents;
 
 [HarmonyPatch(typeof(SimGameState))]
-[HarmonyPatch("GetMechComponentRefForUID")]
+[HarmonyPatch(nameof(SimGameState.GetMechComponentRefForUID))]
 public static class SimGameState_GetMechComponentRefForUID
 {
     [HarmonyPrefix]
@@ -15,13 +15,14 @@ public static class SimGameState_GetMechComponentRefForUID
             return;
         }
 
-        Log.ComponentInstall.Trace?.Log($"Prefix: Get ref for mech:{mech.Description.Id} suid:{simGameUID} cid:{componentID} type:{componentType}");
+        Log.ComponentInstall.Trace?.Log($"Prefix: Get ref for mech:{mech?.Description?.Id} suid:{simGameUID} cid:{componentID} type:{componentType}");
     }
 
     [HarmonyPostfix]
+    [HarmonyWrapSafe]
     public static void Postfix(MechDef mech, string simGameUID, string componentID, ComponentType componentType, MechComponentRef __result, SimGameState __instance)
     {
-        Log.ComponentInstall.Trace?.Log($"Postfix: Get ref for mech:{mech.Description.Id} suid:{simGameUID} cid:{componentID} type:{componentType}");
+        Log.ComponentInstall.Trace?.Log($"Postfix: Get ref for mech:{mech?.Description?.Id} suid:{simGameUID} cid:{componentID} type:{componentType}");
 
         if (__result == null)
         {
