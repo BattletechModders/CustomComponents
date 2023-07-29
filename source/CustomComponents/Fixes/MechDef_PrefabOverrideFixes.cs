44b449187e239c0e6559a8618a555a44499b4ae0
CptMoore <39010654+CptMoore@users.noreply.github.com>
2023-07-29 15:37:27 +0200

    PrefabOverridesCache code cleanup

--- source/CustomComponents/Fixes/MechDef_PrefabOverrideFixes.cs
+++ source/CustomComponents/Fixes/MechDef_PrefabOverrideFixes.cs
@@ -24,12 +24,12 @@ internal static class MechDef_PrefabOverrideFixes
         {
             if (PrefabOverridesCache.Remove(mechDef.chassisID))
             {
-                Log.PrefabOverrideCache.Debug?.Log($"Removed chassisID {mechDef.chassisID}");
+                Log.PrefabOverrideCache.Debug?.Log($"Removed prefabOverride for {mechDef.chassisID}");
             }
             return;
         }
 
-        Log.PrefabOverrideCache.Debug?.Log($"Added prefabOverride {mechDef.prefabOverride} for chassisID {mechDef.chassisID}");
+        Log.PrefabOverrideCache.Debug?.Log($"Adding prefabOverride {mechDef.prefabOverride} for {mechDef.chassisID}");
         PrefabOverridesCache[mechDef.chassisID] = mechDef.prefabOverride;
     }
 
@@ -39,22 +39,23 @@ internal static class MechDef_PrefabOverrideFixes
     internal static void MechDef_set_Chassis_Prefix(MechDef __instance, ChassisDef __0)
     {
         var mechDef = __instance;
+        var chassisDef = __0;
+
         if (!string.IsNullOrEmpty(mechDef.prefabOverride))
         {
             return;
         }
 
-        var chassisId = __0?.Description?.Id ?? __instance.chassisID;
+        var chassisId = chassisDef?.Description?.Id ?? __instance.chassisID;
         if (chassisId == null)
         {
             // apparently can happen during json loading if chassis is set to null after prefabOverride was already set
-            // Log.PrefabOverrideCache.Warning?.Log($"Chassis and ChassisId missing for MechDef {mechDef.Description?.Id}");
             return;
         }
 
         if (PrefabOverridesCache.TryGetValue(chassisId, out var prefabOverride))
         {
-            Log.PrefabOverrideCache.Trace?.Log($"Found prefabOverride {prefabOverride} for ChassisId {chassisId}");
+            Log.PrefabOverrideCache.Trace?.Log($"Found prefabOverride {prefabOverride} for {chassisId}");
             mechDef.prefabOverride = prefabOverride;
         }
     }
